cc839d7dccfec96af34728d4f8d37abca64475e1
dinosaurbytes <37344044+dinosaurbytes@users.noreply.github.com>
2018-07-16 07:56:32 -0600

    Created a connection and some classes

---
+++ TravelExpertsAPP/TravelExpertsAPP/Product_Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsAPP
+{
+    public class Product_Supplier
+    {
+        public Product_Supplier() { }
+
+    }
+}
---
+++ TravelExpertsAPP/TravelExpertsAPP/Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsAPP
+{
+    public class Supplier
+    {
+        public Supplier() { }
+
+    }
+}
