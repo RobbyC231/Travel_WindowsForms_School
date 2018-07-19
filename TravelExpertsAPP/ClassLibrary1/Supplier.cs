4a49396436256826bb8d1175e1f134eb2e7b4b5d
ACDM\783907 <snehapatel882@gmail.com>
2018-07-19 09:53:59 -0600

    supplier class
    Create entity class supplier with properties

--- TravelExpertsAPP/ClassLibrary1/Supplier.cs
+++ TravelExpertsAPP/ClassLibrary1/Supplier.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace TravelExpertsLibrary
 {
+    // Business(Entity) Class for Supplier - Middle layer
     public class Supplier
     {
         public Supplier() { }
+
+        public int SupplierId { get; set; }
+
+        public string SupName { get; set; }
 
     }
 }
