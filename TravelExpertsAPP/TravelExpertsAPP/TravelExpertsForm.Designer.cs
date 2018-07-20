9ef688ffeaaca1fff8c004f6ab4f0063c3a07b89
dinosaurbytes <37344044+dinosaurbytes@users.noreply.github.com>
2018-07-20 14:12:50 -0600

    Created GUI for the travel experts form

--- TravelExpertsAPP/TravelExpertsAPP/TravelExpertsForm.Designer.cs
+++ TravelExpertsAPP/TravelExpertsAPP/TravelExpertsForm.Designer.cs
@@ -28,12 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Products = new System.Windows.Forms.TabPage();
+            this.Packages = new System.Windows.Forms.TabPage();
+            this.Suppliers = new System.Windows.Forms.TabPage();
+            this.Product_Suppliers = new System.Windows.Forms.TabPage();
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.btnModifyProduct = new System.Windows.Forms.Button();
+            this.btnDeleteProduct = new System.Windows.Forms.Button();
+            this.btnDeletePackage = new System.Windows.Forms.Button();
+            this.btnModifyPackage = new System.Windows.Forms.Button();
+            this.btnAddPackage = new System.Windows.Forms.Button();
+            this.btnDeleteSupplier = new System.Windows.Forms.Button();
+            this.btnModifySupplier = new System.Windows.Forms.Button();
+            this.btnAddSupplier = new System.Windows.Forms.Button();
+            this.btnDeleteProductSupplier = new System.Windows.Forms.Button();
+            this.btnModifyProductSupplier = new System.Windows.Forms.Button();
+            this.btnAddProductSupplier = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.Products.SuspendLayout();
+            this.Packages.SuspendLayout();
+            this.Suppliers.SuspendLayout();
+            this.Product_Suppliers.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Products);
+            this.tabControl1.Controls.Add(this.Packages);
+            this.tabControl1.Controls.Add(this.Suppliers);
+            this.tabControl1.Controls.Add(this.Product_Suppliers);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(795, 396);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Products
+            // 
+            this.Products.Controls.Add(this.btnDeleteProduct);
+            this.Products.Controls.Add(this.btnModifyProduct);
+            this.Products.Controls.Add(this.btnAddProduct);
+            this.Products.Location = new System.Drawing.Point(4, 22);
+            this.Products.Name = "Products";
+            this.Products.Padding = new System.Windows.Forms.Padding(3);
+            this.Products.Size = new System.Drawing.Size(787, 370);
+            this.Products.TabIndex = 0;
+            this.Products.Text = "Products";
+            this.Products.UseVisualStyleBackColor = true;
+            // 
+            // Packages
+            // 
+            this.Packages.Controls.Add(this.btnDeletePackage);
+            this.Packages.Controls.Add(this.btnModifyPackage);
+            this.Packages.Controls.Add(this.btnAddPackage);
+            this.Packages.Location = new System.Drawing.Point(4, 22);
+            this.Packages.Name = "Packages";
+            this.Packages.Padding = new System.Windows.Forms.Padding(3);
+            this.Packages.Size = new System.Drawing.Size(787, 370);
+            this.Packages.TabIndex = 1;
+            this.Packages.Text = "Packages";
+            this.Packages.UseVisualStyleBackColor = true;
+            // 
+            // Suppliers
+            // 
+            this.Suppliers.Controls.Add(this.btnDeleteSupplier);
+            this.Suppliers.Controls.Add(this.btnModifySupplier);
+            this.Suppliers.Controls.Add(this.btnAddSupplier);
+            this.Suppliers.Location = new System.Drawing.Point(4, 22);
+            this.Suppliers.Name = "Suppliers";
+            this.Suppliers.Size = new System.Drawing.Size(787, 370);
+            this.Suppliers.TabIndex = 2;
+            this.Suppliers.Text = "Suppliers";
+            this.Suppliers.UseVisualStyleBackColor = true;
+            // 
+            // Product_Suppliers
+            // 
+            this.Product_Suppliers.Controls.Add(this.btnDeleteProductSupplier);
+            this.Product_Suppliers.Controls.Add(this.btnModifyProductSupplier);
+            this.Product_Suppliers.Controls.Add(this.btnAddProductSupplier);
+            this.Product_Suppliers.Location = new System.Drawing.Point(4, 22);
+            this.Product_Suppliers.Name = "Product_Suppliers";
+            this.Product_Suppliers.Size = new System.Drawing.Size(787, 370);
+            this.Product_Suppliers.TabIndex = 3;
+            this.Product_Suppliers.Text = "Product_Suppliers";
+            this.Product_Suppliers.UseVisualStyleBackColor = true;
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Location = new System.Drawing.Point(208, 345);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(75, 23);
+            this.btnAddProduct.TabIndex = 0;
+            this.btnAddProduct.Text = "Add";
+            this.btnAddProduct.UseVisualStyleBackColor = true;
+            // 
+            // btnModifyProduct
+            // 
+            this.btnModifyProduct.Location = new System.Drawing.Point(345, 345);
+            this.btnModifyProduct.Name = "btnModifyProduct";
+            this.btnModifyProduct.Size = new System.Drawing.Size(75, 23);
+            this.btnModifyProduct.TabIndex = 1;
+            this.btnModifyProduct.Text = "Modify";
+            this.btnModifyProduct.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteProduct
+            // 
+            this.btnDeleteProduct.Location = new System.Drawing.Point(489, 345);
+            this.btnDeleteProduct.Name = "btnDeleteProduct";
+            this.btnDeleteProduct.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteProduct.TabIndex = 2;
+            this.btnDeleteProduct.Text = "Delete";
+            this.btnDeleteProduct.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletePackage
+            // 
+            this.btnDeletePackage.Location = new System.Drawing.Point(489, 344);
+            this.btnDeletePackage.Name = "btnDeletePackage";
+            this.btnDeletePackage.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletePackage.TabIndex = 6;
+            this.btnDeletePackage.Text = "Delete";
+            this.btnDeletePackage.UseVisualStyleBackColor = true;
+            // 
+            // btnModifyPackage
+            // 
+            this.btnModifyPackage.Location = new System.Drawing.Point(345, 344);
+            this.btnModifyPackage.Name = "btnModifyPackage";
+            this.btnModifyPackage.Size = new System.Drawing.Size(75, 23);
+            this.btnModifyPackage.TabIndex = 5;
+            this.btnModifyPackage.Text = "Modify";
+            this.btnModifyPackage.UseVisualStyleBackColor = true;
+            // 
+            // btnAddPackage
+            // 
+            this.btnAddPackage.Location = new System.Drawing.Point(208, 344);
+            this.btnAddPackage.Name = "btnAddPackage";
+            this.btnAddPackage.Size = new System.Drawing.Size(75, 23);
+            this.btnAddPackage.TabIndex = 4;
+            this.btnAddPackage.Text = "Add";
+            this.btnAddPackage.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteSupplier
+            // 
+            this.btnDeleteSupplier.Location = new System.Drawing.Point(489, 344);
+            this.btnDeleteSupplier.Name = "btnDeleteSupplier";
+            this.btnDeleteSupplier.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteSupplier.TabIndex = 6;
+            this.btnDeleteSupplier.Text = "Delete";
+            this.btnDeleteSupplier.UseVisualStyleBackColor = true;
+            // 
+            // btnModifySupplier
+            // 
+            this.btnModifySupplier.Location = new System.Drawing.Point(345, 344);
+            this.btnModifySupplier.Name = "btnModifySupplier";
+            this.btnModifySupplier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifySupplier.TabIndex = 5;
+            this.btnModifySupplier.Text = "Modify";
+            this.btnModifySupplier.UseVisualStyleBackColor = true;
+            // 
+            // btnAddSupplier
+            // 
+            this.btnAddSupplier.Location = new System.Drawing.Point(208, 344);
+            this.btnAddSupplier.Name = "btnAddSupplier";
+            this.btnAddSupplier.Size = new System.Drawing.Size(75, 23);
+            this.btnAddSupplier.TabIndex = 4;
+            this.btnAddSupplier.Text = "Add";
+            this.btnAddSupplier.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteProductSupplier
+            // 
+            this.btnDeleteProductSupplier.Location = new System.Drawing.Point(489, 344);
+            this.btnDeleteProductSupplier.Name = "btnDeleteProductSupplier";
+            this.btnDeleteProductSupplier.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteProductSupplier.TabIndex = 6;
+            this.btnDeleteProductSupplier.Text = "Delete";
+            this.btnDeleteProductSupplier.UseVisualStyleBackColor = true;
+            // 
+            // btnModifyProductSupplier
+            // 
+            this.btnModifyProductSupplier.Location = new System.Drawing.Point(345, 344);
+            this.btnModifyProductSupplier.Name = "btnModifyProductSupplier";
+            this.btnModifyProductSupplier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifyProductSupplier.TabIndex = 5;
+            this.btnModifyProductSupplier.Text = "Modify";
+            this.btnModifyProductSupplier.UseVisualStyleBackColor = true;
+            // 
+            // btnAddProductSupplier
+            // 
+            this.btnAddProductSupplier.Location = new System.Drawing.Point(208, 344);
+            this.btnAddProductSupplier.Name = "btnAddProductSupplier";
+            this.btnAddProductSupplier.Size = new System.Drawing.Size(75, 23);
+            this.btnAddProductSupplier.TabIndex = 4;
+            this.btnAddProductSupplier.Text = "Add";
+            this.btnAddProductSupplier.UseVisualStyleBackColor = true;
+            // 
+            // TravelExpertsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(795, 396);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "TravelExpertsForm";
             this.Text = "TravelExpertsForm";
+            this.tabControl1.ResumeLayout(false);
+            this.Products.ResumeLayout(false);
+            this.Packages.ResumeLayout(false);
+            this.Suppliers.ResumeLayout(false);
+            this.Product_Suppliers.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Products;
+        private System.Windows.Forms.TabPage Packages;
+        private System.Windows.Forms.Button btnDeleteProduct;
+        private System.Windows.Forms.Button btnModifyProduct;
+        private System.Windows.Forms.Button btnAddProduct;
+        private System.Windows.Forms.Button btnDeletePackage;
+        private System.Windows.Forms.Button btnModifyPackage;
+        private System.Windows.Forms.Button btnAddPackage;
+        private System.Windows.Forms.TabPage Suppliers;
+        private System.Windows.Forms.Button btnDeleteSupplier;
+        private System.Windows.Forms.Button btnModifySupplier;
+        private System.Windows.Forms.Button btnAddSupplier;
+        private System.Windows.Forms.TabPage Product_Suppliers;
+        private System.Windows.Forms.Button btnDeleteProductSupplier;
+        private System.Windows.Forms.Button btnModifyProductSupplier;
+        private System.Windows.Forms.Button btnAddProductSupplier;
     }
 }
